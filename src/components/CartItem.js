3f2e7e2b409ad50107402708243f0123697d43f3
Riley Sherlock <riley.sherlock@mts.net>
2022-06-14 14:44:15 -0500

    style CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+import '../styles/CartItem.css';
+
 export default function CartItem(props) {
+  let [count, setCount] = useState(0);
+
+  function handleChange(e) {
+    setCount(e.target.value);
+  }
+
   return (
     <div className="CartItem">
       <span>{props.name}</span>
-      <span>{props.count}</span>
-      <button>+</button>
-      <button>-</button>
-      <button>x</button>
+      <div>
+        <input
+          type="number"
+          name=""
+          id=""
+          value={count}
+          onChange={handleChange}
+        />
+        <button>x</button>
+      </div>
     </div>
   );
 }
