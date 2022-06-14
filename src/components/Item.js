4d708127273b65f9f65dd9a3ca384dd5bb31d2c6
Riley Sherlock <riley.sherlock@mts.net>
2022-06-14 14:07:16 -0500

    add styling to item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,9 @@
+import '../styles/Item.css';
+
 export default function Item(props) {
   return (
-    <div>
-      <img src={props.image} alt="" srcset="" />
+    <div className="Item">
+      <img src={props.image} alt="" srcSet="" />
       <div className="">{props.name}</div>
       <button>Add to Cart</button>
     </div>
