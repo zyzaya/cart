b92c7f285a03522ee2710be2491a629652675c87
Riley Sherlock <riley.sherlock@mts.net>
2022-06-20 14:39:22 -0500

    implement items display on Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,24 @@
 import '../styles/Home.css';
 import { faker } from '@faker-js/faker';
 import Navbar from './Navbar';
-import Cart from './Cart';
 import uniqid from 'uniqid';
+import Item from './Item';
 
 function Home() {
   let items = [...new Array(15)].map((e) => ({
     name: faker.name.firstName(),
     key: uniqid(),
+    image: faker.image.abstract(),
   }));
 
   return (
-    <div className="App">
+    <div className="Home">
       <Navbar />
-      <Cart items={items} />
+      <div className="Items">
+        {items.map((i) => (
+          <Item key={i.key} name={i.name} image={i.image} />
+        ))}
+      </div>
     </div>
   );
 }
