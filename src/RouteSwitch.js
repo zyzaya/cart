542d65f1d916d266ca5983e3b9564e744e84dc7d
Riley Sherlock <riley.sherlock@mts.net>
2022-06-20 14:30:52 -0500

    rename App to Home

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
+import Home from './components/Home';
 import Cart from './components/Cart';
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
-import './App.css';
+import '../styles/Home.css';
 import { faker } from '@faker-js/faker';
-import Navbar from './components/Navbar';
-import Cart from './components/Cart';
+import Navbar from './Navbar';
+import Cart from './Cart';
 import uniqid from 'uniqid';
 
-function App() {
+function Home() {
   let items = [...new Array(15)].map((e) => ({
     name: faker.name.firstName(),
     key: uniqid(),
@@ -18,4 +18,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
