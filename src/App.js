1e84d5fef8b58ac608f83e3518f71b3a230c88ae
Riley Sherlock <riley.sherlock@mts.net>
2022-06-20 14:17:25 -0500

    add react-routerd-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import { faker } from '@faker-js/faker';
-import Item from './components/Item';
 import Navbar from './components/Navbar';
 import Cart from './components/Cart';
 import uniqid from 'uniqid';
 
 function App() {
-  let image = faker.image.abstract();
-  let name = faker.name.firstName();
   let items = [...new Array(15)].map((e) => ({
     name: faker.name.firstName(),
     key: uniqid(),
@@ -16,7 +13,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Item image={image} name={name} /> */}
       <Cart items={items} />
     </div>
   );
