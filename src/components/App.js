2a4c994acb92cddf63530727bc18083a536f4ecf
Riley Sherlock <riley.sherlock@mts.net>
2022-06-20 15:15:09 -0500

    move item list creation from Home to App

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,16 @@ import { Outlet, Route, Routes } from 'react-router-dom';
 import Cart from './Cart';
 import Home from './Home';
 import Navbar from './Navbar';
+import { faker } from '@faker-js/faker';
+import uniqid from 'uniqid';
 
 export default function App() {
+  let items = [...new Array(15)].map((e) => ({
+    name: faker.name.firstName(),
+    key: uniqid(),
+    image: faker.image.abstract(640, 480, true),
+  }));
+
   function handleAddToCart(item) {
     console.log(item);
   }
@@ -13,7 +21,10 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="cart" element={<Cart />} />
-        <Route path="" element={<Home onAddToCart={handleAddToCart} />} />
+        <Route
+          path=""
+          element={<Home onAddToCart={handleAddToCart} items={items} />}
+        />
       </Routes>
       <Outlet onAddToCart={handleAddToCart} />
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,11 @@
 import '../styles/Home.css';
-import { faker } from '@faker-js/faker';
-import uniqid from 'uniqid';
 import Item from './Item';
 
 function Home(props) {
-  console.log(props);
-  let items = [...new Array(15)].map((e) => ({
-    name: faker.name.firstName(),
-    key: uniqid(),
-    image: faker.image.abstract(640, 480, true),
-  }));
-
   return (
     <div className="Home">
       <div className="Items">
-        {items.map((i) => (
+        {props.items.map((i) => (
           <Item
             key={i.key}
             name={i.name}
